c3247264766d2f5eb2c911cd0576d88ce03dd46b
Jon Xie <Weak-Chicken@users.noreply.github.com>
2019-04-29 13:33:58 +0800

    about page

--- Day6 & 7/my_CNode_practice/my_cnode_practice/src/views/About.vue
+++ Day6 & 7/my_CNode_practice/my_cnode_practice/src/views/About.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <br>
+    <p class="about-content">Author: Yucheng Xie</p>
+    <p class="about-content">This project is inspired by SD-Gaming's CNode copy project. And many materials are taken from that project.</p>
+    <p class="about-content">Please check out his/her <a href="https://github.com/SD-Gaming/Vue2.0_CNode.bbs">github page</a> for more details</p>
+    <p class="about-content">Also please check out the <a href="https://github.com/Weak-Chicken/one_hundred_days_js_coding/tree/master/Day6%20%26%207/my_CNode_practice/my_cnode_practice">github page</a> of this project for more infomation</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'about',
-  
+
 }
 </script>
+
+<!--TODO: why cannot use scope here-->
+<style>
+  .about {
+    text-align: center;
+  }
+  .about-content {
+    margin-top: 12px;
+  }
+</style>

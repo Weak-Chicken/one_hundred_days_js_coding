265850693ae169daec4d6c191f89746072cb9631
Jon Xie <Weak-Chicken@users.noreply.github.com>
2019-04-29 21:57:47 +0800

    getting data from cnode api

---
+++ Day6 & 7/my_CNode_practice/my_cnode_practice/src/components/Article.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="article">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: Article,
+}
+</script>
+
+<style scoped>
+
+</style>
--- Day6 & 7/my_CNode_practice/my_cnode_practice/src/router.js
+++ Day6 & 7/my_CNode_practice/my_cnode_practice/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
 
 Vue.use(Router)
 
@@ -16,6 +16,6 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About
-    }
+    },
   ]
 })

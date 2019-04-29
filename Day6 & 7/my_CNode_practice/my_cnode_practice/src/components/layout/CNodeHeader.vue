57f4bc3286bce117348bcd38f254935f41a013d5
Jon Xie <Weak-Chicken@users.noreply.github.com>
2019-04-29 13:19:03 +0800

    added header

---
+++ Day6 & 7/my_CNode_practice/my_cnode_practice/src/components/layout/CNodeHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="cnodeHeader">
+    <div id="nav" class="Header">
+      <router-link to="/">
+        <img src="@/assets/cnodejs_light.svg"/>
+      </router-link>
+      <span class="toRight"><router-link to="/about">About</router-link></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cnodeHeader',
+}
+</script>
+
+<!--Copied from [SD-Gaming](https://github.com/SD-Gaming/Vue2.0_CNode.bbs)-->
+<style scoped>
+	.Header {
+		background:#5a5555 ;
+		height: 50px;
+	}
+	a {
+		text-decoration: none;
+		color: #cbc9c9;
+	}
+	img {
+		max-width: 120px;
+		margin-left: 50px;
+		margin-top: 10px;
+	}
+	.toRight {
+		float: right;
+		margin-right: 50px;
+		margin-top: 15px;
+	}
+</style>
